0e923aa9aeab0010f477c97b8026fee4cce3007a
jonh14lk <jvaaa@ic.ufal.br>
2022-01-17 16:40:55 -0300

    adicionando encadeamento para frente

---
+++ ForwardChaining/main.py
@@ -0,0 +1,131 @@
+rules_file_path = "./input/rules.txt"
+facts_file_path = "./input/facts.txt"
+
+val = {}  # valor de um item ou "?" caso seja indefinido
+rules = {}  # dicionário que guarda todas as regras nas quais o item x com valor y está presente como uma clausula
+rule = []  # lista que guarda todas as regras
+
+
+def check_item(item):
+    if item not in val:
+        val[item] = "?"
+        rules[item] = []
+
+
+def set_fact(fact, value):
+    check_item(fact)
+    val[fact] = value
+
+
+def add_rule(item, index):
+    check_item(item)
+    rules[item].append(index)
+
+
+def parse_line(words):
+    conditions = []
+    conclusion = ("", "")
+    i = 0
+
+    while i < len(words):
+        if words[i] == "se" or words[i] == "e":
+            conditions.append((words[i + 1], words[i + 3]))
+            i += 4
+        else:
+            conclusion = (words[i + 1], words[i + 3])
+            i += 4
+
+    return (conditions, conclusion)
+
+
+def read_rules():
+    lines = []
+
+    with open(rules_file_path) as file:
+        lines = file.readlines()
+
+    index = 0
+
+    for l in lines:
+        parsed = parse_line(l.split())
+        conditions = parsed[0]
+        conclusion = parsed[1]
+        rule.append((conditions, conclusion))
+
+        for c in conditions:
+            add_rule(c, index)
+
+        check_item(conclusion)
+        index += 1
+
+    print("Regras:")
+    for item in rule:
+        j = 0
+
+        while j < len(item[0]):
+            print(str(item[0][j][0]) + " = " + str(item[0][j][1]), end="")
+            print("" if j == len(item[0]) - 1 else " e ", end="")
+            j += 1
+
+        print(" entao ", end="")
+        print(str(item[1][0]) + " = " + str(item[1][1]))
+
+    print()
+
+
+def read_facts():
+    lines = []
+
+    with open(facts_file_path) as file:
+        lines = file.readlines()
+
+    for l in lines:
+        fact = l.split()
+        set_fact(fact[0], fact[2])
+
+    print("Fatos:")
+    for item in val.keys():
+        if val[item] != "?":
+            print(str(item) + " = " + str(val[item]))
+    print()
+
+
+def check_rule(r):
+    for item in rule[r][0]:
+        if val[item[0]] != item[1]:
+            return False
+
+    print("Aplicando a regra " + str(r) + ": " +
+          str(rule[r][1][0]) + " = " + str(rule[r][1][1]))
+    return True
+
+
+def bfs():
+    used = [False] * len(rule)
+    queue = []
+
+    for item in val.keys():
+        if val[item] != "?":
+            queue.append((item, val[item]))
+
+    while queue:
+        item = queue.pop(0)
+        check_item(item)
+        for r in rules[item]:
+            if used[r]:
+                continue
+
+            if check_rule(r):
+                used[r] = 1
+                val[rule[r][1][0]] = rule[r][1][1]
+                queue.append(rule[r][1])
+
+
+def main():
+    read_rules()
+    read_facts()
+    bfs()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=6717165&assignment_repo_type=AssignmentRepo)
+# Lista 1 de I.A (IC-UFAL)
+
+Aluno: João Victor de Alarcão Ayalla Alcântara
+
+## Formato do input
+
+Para o conjunto de regras, as seguintes condições precisam ser atendidas:
+
+- As regras devem estar contidar no arquivo rules.txt (dentro da pasta input)
+- Uma regra por linha
+- Uma regra deve iniciar pela palavra reservada "se" seguido de uma ou mais condições
+- Cada condição tem a forma: "X = Y", sendo X e Y palavras representando chave e valor respectivamente
+- As condições devem ser concatenadas utilizando a palavra reservada "e"
+- Por último deve vir a palavra reservada "entao" seguido de uma expressão da forma: "X = Y", sendo X e Y palavras representando chave e valor respectivamente
+- Cada palavra deve estar separada por espaços
+- Uma palavra não deve conter espaços
+
+![Exemplo Regras](Screenshots/rules.png)
+
+Para o conjunto de fatos, as seguintes condições precisam ser atendidas:
+
+- Os fatos devem estar contidos no arquivo facts.txt (dentro da pasta input)
+- Um fato por linha
+- Um fato tem a forma: "X = Y", sendo X e Y palavras representando chave e valor respectivamente
+
+![Exemplo Fatos](Screenshots/facts.png)
+
+# Execução
+
+Para a execução, deve se executar o arquivo main.py da pasta selecionada, através do comando 'python3 main.py'. Cada pasta contém uma estratégia de encadeamento.
+
+![Exemplo Execução](Screenshots/execution.png)
